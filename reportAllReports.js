751e2f3b2f49ca51f3375fe0def207895f5b35dc
Tim Dobson <hello@tdobson.net>
2024-10-15 11:17:14 +0100

    fix: Refactor `readData` function to use consistent indentation

--- reportAllReports.js
+++ reportAllReports.js
@@ -1,23 +1,22 @@
 function readData() {
-  var conn = Jdbc.getConnection(url, username, password);
-  var stmt = conn.createStatement();
+	var conn = Jdbc.getConnection(url, username, password);
+	var stmt = conn.createStatement();
 
-  const reports = [
-    readOutput,
-    readVolunteers,
-    readLeadBelayTraining,
-    readEventListing,
-    readBadgesNeeded,
-    readBandsNeeded,
-    readTopRopeTraining,
-    readStats,
-    readRoles,
-    readVolunteerIntent,
-  ];
+	const reports = [
+		readOutput,
+		readVolunteers,
+		readLeadBelayTraining,
+		readEventListing,
+		readBadgesNeeded,
+		readBandsNeeded,
+		readTopRopeTraining,
+		readStats,
+		readRoles,
+		readVolunteerIntent,
+	];
 
-  reports.forEach(report => report(stmt));
+	reports.forEach((report) => report(stmt));
 
-  stmt.close();
-  conn.close();
+	stmt.close();
+	conn.close();
 }
-
