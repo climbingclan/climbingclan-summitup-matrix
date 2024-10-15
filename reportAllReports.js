2022d563ded6316207d742b36359383c4edf7ac6
Tim Dobson <hello@tdobson.net>
2024-10-15 11:18:40 +0100

    style: Ran the linter

--- reportAllReports.js
+++ reportAllReports.js
@@ -1,24 +1,24 @@
 function readData() {
-  const conn = Jdbc.getConnection(url, username, password);
-  const stmt = conn.createStatement();
+	const conn = Jdbc.getConnection(url, username, password);
+	const stmt = conn.createStatement();
 
-  const reports = [
-    readOutput,
-    readVolunteers,
-    readLeadBelayTraining,
-    readEventListing,
-    readBadgesNeeded,
-    readBandsNeeded,
-    readTopRopeTraining,
-    readStats,
-    readRoles,
-    readVolunteerIntent,
-  ];
+	const reports = [
+		readOutput,
+		readVolunteers,
+		readLeadBelayTraining,
+		readEventListing,
+		readBadgesNeeded,
+		readBandsNeeded,
+		readTopRopeTraining,
+		readStats,
+		readRoles,
+		readVolunteerIntent,
+	];
 
-  for (const report of reports) {
-    report(stmt);
-  }
+	for (const report of reports) {
+		report(stmt);
+	}
 
-  stmt.close();
-  conn.close();
+	stmt.close();
+	conn.close();
 }
